e09db20abd3bc35e61149fd552f3fb001844d5e4
Justin Traglia <jtraglia@methamphetamine.home>
2021-01-25 16:44:06 -0600

    Remove Asset class, add Withdrawal class

--- source/account.py
+++ source/account.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3
 
-class Asset(object):
+class Withdrawal:
     """
-    This class represents an asset.
+    This class represents a withdrawal. It stores relevant information.
+    Withdrawals can be insufficient.
     """
-    def __init__(self, contribution):
-        self.age = 0
-        self.basis = contribution
-        self.value = contribution
+    def __init__(self, value, gains, insufficient=0):
+        self.value = value
+        self.gains = gains
+        self.insufficient = insufficient
 
     def __repr__(self):
-        return f"Asset(age={self.age:d} basis={self.basis=:.2f} value={self.value=:.2f})"
-
-    def get_basis(self):
-        return self.basis
+        return (
+            f"Withdrawal(value={self.value:,.2f}"
+            + f" gains={self.gains:,.2f}"
+            + (f" insufficient={self.insufficient:,.2f}" if self.insufficient else "")
+            + ")"
+        )
 
     def get_value(self):
         return self.value
 
     def get_gains(self):
-        return self.value - self.basis
+        return self.gains
 
-    def increment(self, interest_rate):
-        self.age += 1
-        self.value *= interest_rate
-        self.value = round(self.value, 2)
-
+    def get_insufficient(self):
+        return self.needed
 
 class Account:
     """
@@ -35,26 +35,30 @@ class Account:
         self.rate_of_return = rate_of_return
         self.withdrawal_contributions_first = withdrawal_contributions_first
         self.account_age = 0
-        self.assets = []
+        self.contributions = 0
+        self.value = 0
 
     def __repr__(self):
-        lines = []
-        lines.append(f"Account(age={self.account_age:d} value={self.get_value():,.2f} gains={self.get_gains():,.2f})")
-        for asset in self.assets:
-            lines.append("\t" + repr(asset))
-        return "\n".join(lines)
+        return (
+            f"Account(age={self.account_age:d}"
+            f" value={self.get_value():,.2f}"
+            f" gains={self.get_gains():,.2f}"
+            f" contrs_first={self.withdrawal_contributions_first}"
+            f")"
+        )
 
     def contribute(self, money):
-        self.assets.insert(0, Asset(money))
+        self.contributions += money
+        self.value += money
 
     def get_value(self):
-        return sum(a.get_value() for a in self.assets)
+        return self.value
 
     def get_contributions(self):
-        return sum(a.get_basis() for a in self.assets)
+        return self.contributions
 
     def has_contributions(self):
-        return bool(self.get_contributions())
+        return bool(self.contributions)
 
     def get_gains(self):
         return self.get_value() - self.get_contributions()
@@ -69,7 +73,6 @@ class Account:
         total_gains = 0
 
         while round(still_needed, 2):
-
             #
             # If our account has nothing in it, there's no point in trying. Just
             # return what we were able to get, if anything.
@@ -77,44 +80,26 @@ class Account:
             if not self.get_value():
                 break
 
-            for asset in self.assets:
-                to_take = min(still_needed, asset.get_value())
-                if self.withdrawal_contributions_first \
-                        and self.has_contributions():
-                    to_take = min(still_needed, asset.get_basis())
+            to_take = min(still_needed, self.get_value())
+            if self.withdrawal_contributions_first and self.has_contributions():
+                to_take = min(still_needed, self.get_contributions())
 
-                #
-                # TODO: Double check that this is right.
-                #
-                ratio = asset.get_gains()/asset.get_value()
-                if self.withdrawal_contributions_first \
-                        and self.has_contributions():
-                    ratio = 0.0
+            ratio = self.get_gains()/self.get_value()
+            if self.withdrawal_contributions_first and self.has_contributions():
+                ratio = 0.0
 
-                asset.value -= to_take
-                asset.basis -= asset.get_basis() * (1 - ratio)
-                still_needed -= to_take
-                total_taken += to_take
-                total_gains += to_take * ratio
+            self.value -= to_take
+            self.contributions -= to_take * (1 - ratio)
 
-            #
-            # Remove assets that have no value. No point in keeping them around.
-            #
-            non_zero_assets = []
-            for asset in self.assets:
-                if round(asset.get_value(), 2):
-                    non_zero_assets.append(asset)
-            self.assets = non_zero_assets
+            total_taken += to_take
+            total_gains += to_take * ratio
+            still_needed -= to_take
 
-        #
-        # Return the gains too, so we know if we need to pay taxes.
-        #
-        return total_taken, total_gains
+        return Withdrawal(total_taken, total_gains, still_needed)
 
     def increment(self):
         self.account_age += 1
-        for asset in self.assets:
-            asset.increment(self.rate_of_return)
+        self.value *= self.rate_of_return
 
 
 if __name__ == "__main__":
@@ -128,11 +113,6 @@ if __name__ == "__main__":
     account.increment()
     account.contribute(100.00)
 
-    print("before")
     print(account)
-
-    withdrawal = account.withdrawal(100)
-    print(f"got {withdrawal[0]:,.2f}, gains: {withdrawal[1]:,.2f}")
-
-    print("after")
+    print(account.withdrawal(1500))
     print(account)
